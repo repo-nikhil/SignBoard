4af34659a96ddc398f98c7bbab28e7f7af122c02
Pranav <pranavtiwary738@gmail.com>
2023-09-28 23:11:48 +0530

    .

--- NodeCrud/server.js
+++ NodeCrud/server.js
@@ -21,5 +21,5 @@ app.get("/", (req, res) => {
   // set port, listen for requests
   const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.` );
+    console.log(`My Server is running on port ${PORT}.` );
   });
